ea3bc9ca1d2237e18f0bd481d1bb1af42aa0089b
Pete West <peterjwest3@gmail.com>
2020-09-15 10:54:52 +0100

    Add index test for coverage

---
+++ tests/index.ts
@@ -0,0 +1,20 @@
+import assert from 'assert';
+import multiline from 'multiline-ts';
+
+import yason from '../src/index';
+
+describe('index', () => {
+  it('Decodes a list', () => {
+    const input = multiline`
+      - "Hello \\"World\\""
+      - 123
+      - null
+    `;
+
+    assert.deepStrictEqual(yason.decode(input), [
+      'Hello "World"',
+      123,
+      null,
+    ]);
+  });
+});
